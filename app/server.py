855bd63035cbe82143aeb6147ab081f876ef5c46
Marius Vach <rasmus_oo@hotmail.de>
2019-01-25 12:26:46 +0100

    added my model file

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/f1xpmyig3mtvep0/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Vizsla', 'Weimaraner']
 path = Path(__file__).parent
 
 app = Starlette()
